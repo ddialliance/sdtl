33b2825fd525d687de1879a3a9ff3c93235d418e
Jeremy Iverson <jeremy@colectica.com>
2017-07-21 13:05:30 -0500

    build script

--- build/initialize-environment.ps1
+++ build/initialize-environment.ps1
@@ -1,10 +1,10 @@
-// Download COGS
+# Download COGS
 Start-FileDownload 'https://ci.appveyor.com/api/buildjobs/n95t2byfet043px6/artifacts/Cogs.Console/bin/Release/Windows-CogsRelease.zip'
-7z e Windows-CogsRelease.zip
+7z e Windows-CogsRelease.zip -y
 
-// Download Graphviz
+# Download Graphviz
 Start-FileDownload 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.zip'
-7z e graphviz-2.38.zip
+7z e graphviz-2.38.zip -y
 
-// Download Sphinx
+# Download Sphinx
 pip install sphinx
