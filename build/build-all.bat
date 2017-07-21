ab78dd583a6de2063f3a05eb5bf2718de119c319
Jeremy Iverson <jeremy@colectica.com>
2017-07-21 13:09:11 -0500

    build script

--- build/build-all.bat
+++ build/build-all.bat
@@ -1,17 +1,17 @@
 mkdir out\
 
 REM Validate the model.
-dotnet run -- validate .
+dotnet Cogs.Console.dll validate .
 
 REM Build all the COGS outputs.
-dotnet run -- publish-json . out\json --overwrite
-dotnet run -- publish-graphql . out\graphql --overwrite
-dotnet run -- publish-xsd . out\xsd --overwrite --namespace "http://example.org/sdtl" --namespacePrefix sdtl
-dotnet run -- publish-uml . out\uml release\bin --overwrite
-REM dotnet run -- publish-dot . out\dot release\bin --overwrite --single
-REM dotnet run -- publish-dot . out\dot release\bin --overwrite --all --inheritance
-dotnet run -- publish-sphinx . out\sphinx c:\bin\graphviz\bin --overwrite
-dotnet run -- publish-cs . out\csharp --overwrite
+dotnet Cogs.Console.dll publish-json . out\json --overwrite
+dotnet Cogs.Console.dll publish-graphql . out\graphql --overwrite
+dotnet Cogs.Console.dll publish-xsd . out\xsd --overwrite --namespace "http://example.org/sdtl" --namespacePrefix sdtl
+dotnet Cogs.Console.dll publish-uml . out\uml release\bin --overwrite
+REM dotnet Cogs.Console.dll publish-dot . out\dot release\bin --overwrite --single
+REM dotnet Cogs.Console.dll publish-dot . out\dot release\bin --overwrite --all --inheritance
+dotnet Cogs.Console.dll publish-sphinx . out\sphinx c:\bin\graphviz\bin --overwrite
+dotnet Cogs.Console.dll publish-cs . out\csharp --overwrite
 
 REM Generate documentation with Sphinx.
 PUSHD %sphinxdir%
