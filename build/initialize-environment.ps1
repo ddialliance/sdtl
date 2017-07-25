633bb5a53c6ed94494147d40a56542f03d048af1
Jeremy Iverson <jeremy@colectica.com>
2017-07-25 15:48:30 -0500

    build script

--- build/initialize-environment.ps1
+++ build/initialize-environment.ps1
@@ -10,12 +10,12 @@ Start-FileDownload 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz
 ls graphviz\bin
 
 # Download .NET Core 2.0 Preview 2 SDK and add to PATH
-$urlCurrent = "https://download.microsoft.com/download/F/A/A/FAAE9280-F410-458E-8819-279C5A68EDCF/dotnet-sdk-2.0.0-preview2-006497-win-x64.zip"
-$env:DOTNET_INSTALL_DIR = "$pwd\.dotnetsdk"
-$tempFileCurrent = [System.IO.Path]::GetTempFileName()
-(New-Object System.Net.WebClient).DownloadFile($urlCurrent, $tempFileCurrent)
-Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFileCurrent, $env:DOTNET_INSTALL_DIR)
-$env:Path = "$env:DOTNET_INSTALL_DIR;$env:Path"  
+# $urlCurrent = "https://download.microsoft.com/download/F/A/A/FAAE9280-F410-458E-8819-279C5A68EDCF/dotnet-sdk-2.0.0-preview2-006497-win-x64.zip"
+# $env:DOTNET_INSTALL_DIR = "$pwd\.dotnetsdk"
+# $tempFileCurrent = [System.IO.Path]::GetTempFileName()
+# (New-Object System.Net.WebClient).DownloadFile($urlCurrent, $tempFileCurrent)
+# Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFileCurrent, $env:DOTNET_INSTALL_DIR)
+# $env:Path = "$env:DOTNET_INSTALL_DIR;$env:Path"  
 
 # Download Sphinx
 pip install sphinx
