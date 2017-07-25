111a2eaac42bdb7403a7f650affffca37ae01033
Jeremy Iverson <jeremy@colectica.com>
2017-07-25 16:31:48 -0500

    build script

--- build/build-windows.bat
+++ build/build-windows.bat
@@ -14,13 +14,13 @@ echo XSD
 %cogs% publish-xsd . out\xsd --overwrite --namespace "http://example.org/sdtl" --namespacePrefix sdtl
 
 echo UML
-%cogs% publish-uml . out\uml --location graphviz\ --overwrite
+%cogs% publish-uml . out\uml --location graphviz\release\bin --overwrite
 
-REM %cogs% publish-dot . out\dot graphviz\ --overwrite --single
-REM %cogs% publish-dot . out\dot graphviz\ --overwrite --all --inheritance
+REM %cogs% publish-dot . --location out\dot graphviz\release\bin --overwrite --single
+REM %cogs% publish-dot . --location out\dot graphviz\release\bin --overwrite --all --inheritance
 
 echo Sphinx
-%cogs% publish-sphinx . out\sphinx --location graphviz\ --overwrite
+%cogs% publish-sphinx . out\sphinx --location graphviz\release\bin --overwrite
 
 echo C#
 %cogs% publish-cs . out\csharp --overwrite
