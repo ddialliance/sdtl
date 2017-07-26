56f5b2e21803781bdbfb08548243de8bc7220552
Jeremy Iverson <jeremy@colectica.com>
2017-07-26 10:07:56 -0500

    build script

--- build/deploy-gitlab-pages.ps1
+++ build/deploy-gitlab-pages.ps1
@@ -1,13 +1,14 @@
-REM Push the dirhtml subdirectory to Gitlab Pages
+# Push the dirhtml subdirectory to Gitlab Pages
 PUSHD out\sphinx\build
 git config --global credential.helper store
-Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$env:access_token:x-oauth-basic@github.com`n"
 git config --global user.name "AppVeyor"
 git config --global user.email "build@c2metadata.org"
 git init
 git remote add gitlabpages git@gitlab.com:c2metadata/sdtl-docs.git
-git add dirhtml
+mv dirhtml master
+git add master
 git commit -m 'docs'
-git subtree push --prefix dirhtml gitlabpages master
+git push gitlabpages master
 POPD
 
